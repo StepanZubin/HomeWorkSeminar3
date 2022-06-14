cc1baa601ccd828788d70429bf6dc095633c915d
STEPAN <chichetka27@gmail.com>
2022-06-14 17:45:54 +0400

    add folder P19 Polydrome

---
+++ P_19_Polydrome/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 19. Написать программу, которая принимает 
+на вход пятизначное число и проверяет, является ли 
+оно палиндромом: 14212 -> нет; 12821 -> да  */
+
+Console.Clear();
+Console.Write("Ведите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 99999 || num < 10000)
+{
+    Console.Write("Ошибка вводв!");
+    return;
+}
+
+int num5 = num % 10;
+int num4 = num / 10 % 10;
+// int num3 = num / 100 % 10;
+int num2 = num / 1000 % 10;
+int num1 = num / 10000;
+
+if (num1 == num5 && num2 == num4)
+{
+    Console.Write("ДА");
+}
+else
+{
+    Console.Write("НЕТ");
+}
