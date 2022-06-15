3a3636fbbc349b544028be5103bb14f74c933d30
STEPAN <chichetka27@gmail.com>
2022-06-15 06:42:51 +0400

    add folder P21 Table of cubes

--- P_23_Table_of_cubes/Program.cs
+++ P_23_Table_of_cubes/Program.cs
@@ -3,3 +3,23 @@
 от 1 до N: 3 -> 1, 8, 27. */
 
 Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+if (num < 0)
+{
+        num = num * -1;
+}
+
+while (count <= num)
+{
+    int numCube = Cube(count);
+    count++;
+    Console.WriteLine(numCube);
+}
+
+int Cube(int a) // метод: возведение в куб
+{
+    return a * a * a;
+}
