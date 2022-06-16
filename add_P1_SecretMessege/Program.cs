4539aab135f769861c72bbf6d97778f0d6e838b8
STEPAN <chichetka27@gmail.com>
2022-06-16 15:40:37 +0400

    add folders P1_SecretVessege; P2_InfinitLoop

---
+++ add_P1_SecretMessege/Program.cs
@@ -0,0 +1,48 @@
+﻿// Доп.Задача 1. В переменной string есть секретное сообщение, 
+// во второй - есть пароль. Пользователю программы даётся 
+// 3 попытки ввести пароль и увидеть секретное сообщение.
+
+string secret = "Пешеход был скорее прав, чем жив";
+int password = 1234; 
+
+Console.Clear();
+Console.WriteLine("Введите пароль. 3 попытки: **** ");
+int enter1 = Convert.ToInt32(Console.ReadLine());
+
+if (password == enter1)
+{
+    Console.Clear();
+    Console.Write(secret);
+    return;
+}
+else 
+{
+    Console.Clear();
+    Console.WriteLine("Неверный пароль. 2 попытки: **** ");
+}  
+
+int enter2 = Convert.ToInt32(Console.ReadLine());
+if (password == enter2)
+{
+    Console.Clear();
+    Console.Write(secret);
+    return;
+}
+else 
+{
+    Console.Clear();
+    Console.WriteLine("Неверный пароль. 1 попытка: **** ");
+} 
+
+int enter3 = Convert.ToInt32(Console.ReadLine());
+if (password == enter3)
+{
+    Console.Clear();
+    Console.Write(secret);
+    return;
+}
+else 
+{
+    Console.Clear();
+    Console.Write("Попытки закончились");
+}  
---
+++ add_P2_InfinitLoop/Program.cs
@@ -0,0 +1,14 @@
+﻿// Доп.Задача 2. Есть программа с бесконечным циклом. 
+// Когда пользователь вводит exit программа заканчивается.
+
+
+int count = 0;
+string stop = "exit";
+string text = "";
+
+while (text != stop)
+{
+    count++;
+    Console.WriteLine(count);
+    text = Console.ReadLine();
+} 
