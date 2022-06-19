8a4a105ec1476f900bb6ce08ffe8600bcff0cf4d
STEPAN <chichetka27@gmail.com>
2022-06-19 18:09:59 +0400

    add commands to P2 InfinitLoop

--- add_P2_InfinitLoop/Program.cs
+++ add_P2_InfinitLoop/Program.cs
@@ -1,14 +1,44 @@
-﻿// Доп.Задача 2. Есть программа с бесконечным циклом. 
-// Когда пользователь вводит exit программа заканчивается.
+﻿// Доп.Задача 2(4). Есть программа с бесконечным циклом. 
+// Когда пользователь вводит exit программа заканчивается. 
+// Добавить к программе еще 4 команды (их можно придумать самому). 
+// Например: SetName – Установить имя; Help – вывести список команд; 
+// SetPassword – Установить пароль; SetName – Установить имя
+// WriteName – вывести имя после ввода пароля; Exit – выход
 
 
-int count = 0;
+
 string stop = "exit";
 string text = "";
+int[] password = new int[4];
+string name = "";
 
-while (text != stop)
+Console.Clear();
+
+for (int j = 0; text != stop; j++)
 {
-    count++;
-    Console.WriteLine(count);
+    if (text == "Help")
+    {
+        Console.WriteLine("SetName - Установить Имя");
+        Console.WriteLine("SetPassword - Установить Пароль");
+        Console.WriteLine("Help - Вывести список команд");
+        Console.WriteLine("Edit - Выход");
+    }
+    if (text == "SetPassword")
+    {
+        Console.WriteLine("Введите пароль (4 цифры ): **** ");
+        for (int i = 0; i < password.Length; i++)
+        {
+            Console.Write($"{i + 1}-ю цифрa: ");
+            password[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        
+    }
+    if (text == "SetName")
+    {
+        Console.Write("Введите имя: ");
+        name = Console.ReadLine();
+    } 
+   
+    Console.Write("Введите команду: ");
     text = Console.ReadLine();
-} 
+}  
